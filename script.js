f253a60ea9b9e081a77593559e40914180e4741d
mottiseb770 <mottisebarg@yahoo.fr>
2026-05-03 02:06:44 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function analyser() {
+
+  const input =
+document.getElementById("clientInput").value;
+
+  let resultat =   <h2>Analyse IA</h2>    <p><strong>Contexte :</strong><br>   ${input}</p>    <p><strong>Hypothèses :</strong><br>   - problème réseau<br>   - problème VPN<br>   - saturation serveur</p>    <p><strong>Actions :</strong><br>   - vérifier logs<br>   - vérifier CPU/RAM<br>   - vérifier MFA</p>  ;
+
+document.getElementById("resultat").innerHTML =
+resultat;
+}

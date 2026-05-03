b78da1203181fbdb2ba9d5ce659e4d4ddfac4444
mottiseb770 <mottisebarg@yahoo.fr>
2026-05-03 02:34:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,4 @@ function analyser() {
   const input =
 document.getElementById("clientInput").value;
 
-  let resultat =   <h2>Analyse IA</h2>    <p><strong>Contexte :</strong><br>   ${input}</p>    <p><strong>Hypothèses :</strong><br>   - problème réseau<br>   - problème VPN<br>   - saturation serveur</p>    <p><strong>Actions :</strong><br>   - vérifier logs<br>   - vérifier CPU/RAM<br>   - vérifier MFA</p>  ;
-
-document.getElementById("resultat").innerHTML =
-resultat;
-}
+  let resultat =  let resultat = <h2>Analyse IA</h2>  <p><strong>Contexte :</strong><br> ${input}</p>  <p><strong>Hypothèses :</strong><br> - problème réseau<br> - problème VPN<br> - saturation serveur</p>  <p><strong>Actions :</strong><br> - vérifier logs<br> - vérifier CPU/RAM<br> - vérifier MFA</p>;
